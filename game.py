17ad118a5a21e8c2575a12979396bc81b4a1597f
Igbafe Callistus <you@example.com>
2022-06-12 15:01:48 +0100

    Rock, Paper, Scissor Game.

---
+++ game.py
@@ -0,0 +1,104 @@
+# rock-paper-scissors game
+
+import random
+
+input("Play the Rock, Paper, Scissors! Press Enter to continue..\n")
+
+user_score = 0
+computer_score = 0
+
+# Available choices
+choice_list = ["Rock", "Paper", "Scissors"]
+
+# Game loop
+while True:
+    # User input and CPU input with validation
+
+    # Randomize CPU choice
+    random_index = random.randint(0, 2)
+    computer_choice = choice_list[random_index]
+
+    # Change CPU choice from abbr. to full word
+    if computer_choice == "R":
+        computer_choice = "Rock"
+    elif computer_choice == "P":
+        computer_choice = "Paper"
+    elif computer_choice == "S":
+        computer_choice = "Scissors"
+
+    # User input
+    user_choice = input(
+        "Choose R for Rock, P for Paper, or S for Scissors: \n").upper()
+
+    # Change user choice from abbr. to full word
+    if user_choice == "R":
+        user_choice = "Rock"
+    elif user_choice == "P":
+        user_choice = "Paper"
+    elif user_choice == "S":
+        user_choice = "Scissors"
+
+    # Checking if user input is valid
+    while user_choice not in choice_list:
+        user_choice = input(
+            "That is not a valid choice. Please try again: \n").upper()
+
+    print(f"CPU : {computer_choice}, Player : {user_choice}\n")
+
+# Game logic
+
+    # Tie
+    while user_choice == computer_choice:
+        print("It's a tie!\n")
+        user_choice = input("Please choose again: \n").upper()
+
+        # Change user choice from abbrivation. to full word
+        if user_choice == "R":
+            user_choice = "Rock"
+        elif user_choice == "P":
+            user_choice = "Paper"
+        elif user_choice == "S":
+            user_choice = "Scissors"
+
+        while user_choice not in choice_list:
+            user_choice = input(
+                "That is not a valid choice. Please try again: \n").upper()
+        print(f"computer : {computer_choice}, Player : {user_choice}\n")
+        break
+    if user_choice == "Rock":
+        if computer_choice == "Scissors":
+            print("You win!\n")
+            user_score += 1
+        else:
+            print("You lose!\n")
+            computer_score += 1
+    elif user_choice == "Paper":
+        if computer_choice == "Rock":
+            print("You win!\n")
+            user_score += 1
+        else:
+            print("You lose!\n")
+            computer_score += 1
+    elif user_choice == "Scissors":
+        if computer_choice == "Paper":
+            print("You win!\n")
+            user_score += 1
+        else:
+            print("You lose!\n")
+            computer_score += 1
+
+# Final score
+    print(f"Player score is {user_score} and the CPU score is {computer_score}\n")
+
+# Play again?
+    repeat_choice = input("Would you like to play again? (Y/N): \n").upper()
+    while repeat_choice not in ["Y", "N"]:
+        repeat_choice = input(
+            "That is not a valid choice. Please try again: \n").upper()
+
+    if repeat_choice == "N":
+        break
+
+# End of game
+print(
+    f"\nFinal score is Player: {user_score} and Computer: {computer_score}\n\nThanks for playing! See you next time!")
